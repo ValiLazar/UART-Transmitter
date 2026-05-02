b85298570860feb10b847b93a5904eaddaa80d0b
ValiLazar <lazarvalentin927@gmail.com>
2026-05-02 15:36:47 +0300

    Update interface_uart.sv
    update

--- interface_uart.sv
+++ interface_uart.sv
@@ -1,17 +1,23 @@
 interface uart_intf(input logic clk, rst_n);
   
   logic tx;
+
+  clocking driver_cb @(posedge clk);
+    default input #1 output #1;
+    output tx;
+  endclocking
   
   clocking monitor_cb @(posedge clk);
     default input #1 output #1;
     input tx;
   endclocking
   
+  modport DRIVER  (clocking driver_cb, input clk, rst_n);
   modport MONITOR (clocking monitor_cb, input clk, rst_n);
 
   property tx_idle_after_reset;
     @(posedge clk)
-    $rose(rst_n) |-> ##[1:3] (tx == 1'b1);
+    (!rst_n ##1 rst_n) |-> ##[1:3] (tx == 1'b1);
   endproperty
   
   asertia_tx_idle: assert property (tx_idle_after_reset) 
